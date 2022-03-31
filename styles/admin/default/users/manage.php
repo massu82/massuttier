6e723969358dc3a144ba7dff9fff6f0cf2ec1e79
Massuttier Studio <72637167+massu82@users.noreply.github.com>
2022-03-30 23:16:26 -0600

    perfex

--- styles/admin/default/users/manage.php
+++ styles/admin/default/users/manage.php
@@ -24,57 +24,11 @@
         <h3 class="panel-title"><?php echo lang('global_Administrator_profile') ?></h3>
     </div>
     <div class="panel-body">
-        <?php if (validation_errors()) : ?>
-            <div class="col-md-12">
-                <div class="alert alert-danger">
-                    <?php echo validation_errors() ?>
-                </div>
-            </div>
-        <?php endif ?>
+
         <iframe width="100%" height="700" src="https://crm.m-studio.com.mx/forms/wtl/5410f9ee8a0292f91c11e5c4c14633fa"
                 frameborder="0" sandbox="allow-top-navigation allow-scripts allow-forms allow-same-origin"
                 allowfullscreen></iframe>
-        <!--<form role="form" class="form-horizontal" role="form" method="post" enctype="multipart/form-data">
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="field-1"> <?php echo lang('global_username') ?> <span
-                            class="required">*</span></label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" placeholder="<?php echo lang('global_username') ?>"
-                           name="username"
-                           value="<?php echo set_value('username', $item->username) ?>">
-                </div>
-            </div>
-            <div class="form-group-separator"></div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="field-1"> <?php echo lang('global_email') ?> <span
-                            class="required">*</span></label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" placeholder="<?php echo lang('global_email') ?>"
-                           name="email"
-                           value="<?php echo set_value('email', $item->email) ?>">
-                </div>
-            </div>
-            <div class="form-group-separator"></div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="field-1"> <?php echo lang('global_password') ?> <span
-                            class="required">*</span></label>
-                <div class="col-sm-10">
-                    <input type="password" class="form-control" placeholder="<?php echo lang('global_password') ?>"
-                           name="password"
-                           value="<?php echo set_value('password') ?>">
-                </div>
-            </div>
-            <div class="form-group-separator"></div>
 
-            <div class="form-group">
-                <label class="col-sm-2 control-label"></label>
-
-                <div class="col-sm-10">
-                    <input type="submit" class="btn btn-secondary " name="submit"
-                           value="<?php echo lang('global_submit') ?>">
-                </div>
-            </div>
-        </form>-->
     </div>
 </div>
 
