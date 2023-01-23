9aef61e7901e696ec1e4a6279baa872634d83150
Massuttier Studio <72637167+massu82@users.noreply.github.com>
2023-01-23 14:59:02 -0600

    301

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://massuttier.com.mx/';
+$config['base_url']	= 'https://massuttier.mx/';
 
 /*
 |--------------------------------------------------------------------------
